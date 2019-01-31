a9c2bc445339198982a4d8d25d063966a3162e24
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2019-01-30 21:48:57 -0500

    Implement connection to SpotifyService

--- app/src/main/java/com/tolkiana/spotifyplayer/MainActivity.kt
+++ app/src/main/java/com/tolkiana/spotifyplayer/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.tolkiana.spotifyplayer
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        connectButton.setOnClickListener {
+            SpotifyService.connect(this) {
+                showPlayer()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        SpotifyService.disconnect()
+    }
+
+    private fun showPlayer() {
+        val intent = Intent(this, PlayerActivity::class.java)
+        startActivity(intent)
     }
 }
